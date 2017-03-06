e5aa6d0358289d2f194f3d0ed63b02327dd0e536
Blaise <blaise.carron@etu.unige.ch>
2017-03-07 00:07:49 +0100

    Added local.properties to .gitignore

--- openCVLibrary310/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary310/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\blais\\Projects\\AndroidStudioProjects\\SyndesiApp\\openCVLibrary310\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: /Users/blaise/IdeaProjects/SyndesiApp/openCVLibrary310/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
